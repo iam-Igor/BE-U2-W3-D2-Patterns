b1dde8b0957c97897233308a5c15648f30e695dd
Ygor Garofalo <igorgarofalo@gmail.com>
2024-01-24 16:49:29 +0100

    adds comments

--- src/main/java/ygorgarofalo/BEU2W3D2Patterns/entitiesEs3/Filter.java
+++ src/main/java/ygorgarofalo/BEU2W3D2Patterns/entitiesEs3/Filter.java
@@ -2,15 +2,19 @@ package ygorgarofalo.BEU2W3D2Patterns.entitiesEs3;
 
 public abstract class Filter {
 
+    // riferimento al prossimo filtro della catena
     private Filter nextFilter;
 
     public abstract void check(int amount);
 
 
+    // imposto il prossimo filtro nella catena
     public void setNextFilter(Filter nextFilter) {
         this.nextFilter = nextFilter;
     }
 
+
+    //metodo per gestire i filtri nella catena, nel metodo check passo l'importo
 
     public void next(int amount) {
         if (nextFilter != null) {
